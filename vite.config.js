9c83cf0a0b6b5c771c789fc54f9f485dc03229ef
illiachumak <illia.chumak.2017@gmail.com>
2023-06-08 20:50:24 +0300

    conf

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    
-    https: true,
-    hmr: {
-      host: 'frontend.persprojchat.space',
-      port: 443,
-      protocol: 'wss',
-    },
+    host: true
   },
   plugins: [react()],
 })
+
