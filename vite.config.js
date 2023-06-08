657b2684d417916579591a6ce423056a720354e6
illiachumak <illia.chumak.2017@gmail.com>
2023-06-08 20:56:38 +0300

    conf

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true
+    proxy: {
+      '/api': {
+        target: 'https://backend.persprojchat.space',
+        changeOrigin: true,
+        ws: true,
+      },
+    },
   },
   plugins: [react()],
-})
-
+});
