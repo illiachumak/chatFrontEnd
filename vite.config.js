b0f34413dd20b0ce0d24bfcdfcbbf90fbbab8195
illiachumak <illia.chumak.2017@gmail.com>
2023-06-08 22:51:06 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { sveltekit } from '@sveltejs/kit/vite';
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: true
-  },
-  plugins: [react()],
-})
+
+
+
+/** @type {import('vite').UserConfig} */
+const config = {
+    plugins: [sveltekit(), react()],
+    server: {
+        hmr: {
+            clientPort: 5111
+        }
+    }
+};
+
+export default config;
