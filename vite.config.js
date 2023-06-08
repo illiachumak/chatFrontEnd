01aea7285bc5aa57d28008a88a14b17873a1635d
illiachumak <illia.chumak.2017@gmail.com>
2023-06-08 23:02:18 +0300

    chngs

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { sveltekit } from '@sveltejs/kit/vite';
+
 
 export default defineConfig({
   server: {
     host: true,
-    hmr: {
-      clientPort: 80,
-    },
+  
   },
-  plugins: [react(), sveltekit()],
+  plugins: [react(),],
 });
 
 
